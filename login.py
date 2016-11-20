afb63d2a8d79be044c3c81e7a693e5b2e5f19558
lin2724 <lin2724@msn.com>
2016-11-20 18:16:24 +0800

    complete login
    login test ok

--- login.py
+++ login.py
@@ -25,24 +25,13 @@ class LoginMethod:
         self.cookie_file_path = 'cookie'
         self.username = ''
         self.password = ''
-        self.http_headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:8.0) Gecko/20100101 Firefox/8.0',
-                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                        'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-                        'Accept-Encoding': 'gzip, deflate',
-                        'DNT': '1',
-                        'Referer': 'http://hkpic-forum.xyz/forum.php',
-                        # 'Cookie': 'PHPSESSID=sq9oris1vovpvnou9rl603p5m0; Ovo6_2132_noticeTitle=1; Ovo6_2132_saltkey=MSTWVgAS; Ovo6_2132_lastvisit=1479612304; Ovo6_2132_lastact=1479615904%09forum.php%09',
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                        # 'Content-Length': '113',
-                        # '': '',
-                        }
 
         self.set_get_cookie = set_get_cookie
         self.do_init()
         pass
 
     def __str__(self):
-        return 'username:%s,password:%s, login_url:%s' %(self.username, self.password, self.login_url)
+        return 'username:%s, login_url:%s' %(self.username, self.login_url)
 
     def do_init(self):
         if not os.path.exists(self.config_file_path):
@@ -63,6 +52,21 @@ thread_number = 30')
                 pass
 
     def get_status(self):
+        http_headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:8.0) Gecko/20100101 Firefox/8.0',
+                              }
+        req_test = urllib2.Request(
+            url=self.status_url,
+            headers=http_headers
+        )
+        data = urllib2.urlopen(req_test).read()
+        with open('hk_check_login_result.html', 'w+') as fd:
+            fd.write(data)
+            print ('get satus end')
+        m = re.search('lin2724', data)
+        if m:
+            return True
+        else:
+            return False
         pass
 
     def get_config(self):
@@ -79,6 +83,11 @@ thread_number = 30')
             return None
         pass
 
+    def check_cookie_exist(self):
+        if os.path.exists(self.cookie_file_path):
+            return True
+        return False
+
     def do_login(self):
         login_data = dict()
         login_data['fastloginfield'] = 'username'
@@ -88,7 +97,16 @@ thread_number = 30')
         login_data['quickfoward'] = 'yes'
         login_data['handlekey'] = 'ls'
 
-        if self.set_get_cookie:
+        http_headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:8.0) Gecko/20100101 Firefox/8.0',
+                             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                             'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+                             'Accept-Encoding': 'gzip, deflate',
+                             'DNT': '1',
+                             'Referer': 'http://hkpic-forum.xyz/forum.php',
+                             'Content-Type': 'application/x-www-form-urlencoded;text/html; charset=utf-8',
+                             }
+
+        if self.set_get_cookie or not self.check_cookie_exist():
             cookie_jar2 = cookielib.LWPCookieJar()
             cookie_support2 = urllib2.HTTPCookieProcessor(cookie_jar2)
             opener2 = urllib2.build_opener(cookie_support2, urllib2.HTTPHandler)
@@ -98,16 +116,40 @@ thread_number = 30')
             req_login = urllib2.Request(
                 url=self.login_url,
                 data=login_data,
-                headers=self.http_headers
+                headers=http_headers
             )
             data = urllib2.urlopen(req_login).read()
-            with open('hk_login_resault.html', 'w+') as fd:
+            with open('hk_login_result.html', 'w+') as fd:
                 fd.write(data)
                 print ('login end')
+            if self.get_status():
+                print 'login succeed!'
+                cookie_jar2.save(self.cookie_file_path, ignore_discard=True, ignore_expires=True)
+            else:
+                print 'login fail'
+            pass
+        else:
+            print ('cookie already exist, try login with cookie')
+            cookie_jar = cookielib.LWPCookieJar(self.cookie_file_path)
+            cookie_jar.load(ignore_discard=True, ignore_expires=True)
+            cookie_support = urllib2.HTTPCookieProcessor(cookie_jar)
+            opener = urllib2.build_opener(cookie_support, urllib2.HTTPHandler)
+            urllib2.install_opener(opener)
+            if self.get_status():
+                print 'login succeed!'
+            else:
+                print 'login with cookie fail, try login with password'
+                self.set_get_cookie = True
+                self.do_login()
+
+
+class ScrapImg:
+    def __init__(self):
         pass
 
+
 if __name__ == '__main__':
-    hk_login = LoginMethod(set_get_cookie=True)
+    hk_login = LoginMethod()
     hk_login.get_config()
     hk_login.do_login()
     print hk_login
